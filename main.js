febb7d9f4abf099cfd34ca1d534ca95400ae32ec
Dario0122 <dario.di.cuia@gmail.com>
2021-11-16 15:55:42 +0100

    img contact

---
+++ main.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1'
+            },
+            {
+                name: 'Luisa',
+                avatar: '_2'
+            },
+            {
+                name: 'Fabio',
+                avatar: '_3'
+            },
+            {
+                name: 'Samuele',
+                avatar: '_4'
+            },
+        ]
+    }
+});
